d6c10108d3b348ca5c35b4254f0d21ce36843084
u-ono <oono.yuu@gmail.com>
2017-12-21 14:50:27 +0900

    Final Commit

--- bashes/green_ball.sh
+++ bashes/green_ball.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-roslaunch dxl_armed_turtlebot hsi_color_filter.launch DEFAULT_NAMESPACE:=/camera/depth_registered FILTER_NAME_SUFFIX:=ryokuball INPUT:=points h_min:=-20 h_max:=20 s_min:=120 i_min:=50
+roslaunch dxl_armed_turtlebot hsi_color_filter.launch DEFAULT_NAMESPACE:=/camera/depth_registered FILTER_NAME_SUFFIX:=ryokuball INPUT:=points h_min:=80 h_max:=127 s_min:=27 s_max:=255 i_min:=100 i_max:=255
--- bashes/houjicha.sh
+++ bashes/houjicha.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-roslaunch dxl_armed_turtlebot hsi_color_filter.launch DEFAULT_NAMESPACE:=/camera/depth_registered INPUT:=points FILTER_NAME_SUFFIX:=houjicha h_min:=-20 h_max:=20 s_min:=120 i_min:=50
+roslaunch dxl_armed_turtlebot hsi_color_filter.launch DEFAULT_NAMESPACE:=/camera/depth_registered INPUT:=points FILTER_NAME_SUFFIX:=houjicha h_min:=-36 h_max:=35 s_min:=0 s_max:=70 i_min:=0 i_max:=80
